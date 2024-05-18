286db7f3cbcddd217089d7e3d6d1e35a961f2b46
MAHolzbach <maholzbach@gmail.com>
2024-05-18 12:03:19 -0400

    Setting up get call

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -21,7 +28,14 @@ func headers(w http.ResponseWriter, req *http.Request) {
 }
 
 func getAll(w http.ResponseWriter, req *http.Request) {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db_String := os.Getenv("DB_STRING")
+	fmt.Println("db_String-->", db_String)
 
 	type JsonBody struct {
 		collection string
@@ -36,12 +50,14 @@ func getAll(w http.ResponseWriter, req *http.Request) {
 	}
 
 	jsonData, jsonErr := json.Marshal(bodyJson)
+	fmt.Println("jsonData-->", jsonData)
 
 	if jsonErr != nil {
 		panic(jsonErr)
 	}
 
 	resp, err := http.Post(db_String, "application/json", bytes.NewBuffer(jsonData))
+	fmt.Println("resp-->", resp)
 
 	if err != nil {
 		panic(err)
@@ -53,9 +69,40 @@ func getAll(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
-	http.HandleFunc("/getAll", getAll)
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	MONGO_URI := os.Getenv("MONGO_URI")
+
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+
+	opts := options.Client().ApplyURI(MONGO_URI).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("cards").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/get-all", getAll)
 
 	http.ListenAndServe(":8090", nil)
 }
